883a43ddcf68af10c766fc26e2a28045543e3010
Kayman Brusse <kaymanbrusse@gmail.com>
2021-09-16 18:17:02 -0400

    Basic testing from Chapter 11

---
+++ adder/src/lib.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn assert_macro() {
+        assert!(true);
+    }
+    
+    #[test]
+    #[should_panic(expected = "Oops")]
+    fn please_panic() {
+        panic!("Oops");
+    }
+    
+    #[test]
+    fn result() -> Result<(), String> {
+        Ok(()) 
+        //Err(String::from("Error!"))
+    }
+
+}
